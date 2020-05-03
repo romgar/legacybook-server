24e9f07f8dd2c08a98b249f69e4d9610f5b9bedd
Romain Garrigues <romain@iwoca.co.uk>
2020-05-03 20:02:34 +0100

    Removes pseudo from user creation (artifact of a copy/paste from a previous project)

--- legacybook_server/account/manager.py
+++ legacybook_server/account/manager.py
@@ -3,18 +3,16 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, pseudo, password=None):
+    def create_user(self, email, password=None):
         if not email:
             raise ValueError('Email must be set!')
-        if not pseudo:
-            raise ValueError('Pseudo must be set!')
-        user = self.model(email=email, pseudo=pseudo)
+        user = self.model(email=email)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, pseudo, password):
-        user = self.create_user(email, pseudo, password)
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
