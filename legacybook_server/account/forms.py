0a4d521c4c3736ae8d189f87f079c6fdef8f2897
Romain Garrigues <romain@iwoca.co.uk>
2020-05-05 23:07:48 +0100

    Adds the password as one of the inputs of the user creation form. Also automatically authenticate and redirects the customer to the profile page.

--- legacybook_server/account/forms.py
+++ legacybook_server/account/forms.py
@@ -6,4 +6,11 @@ from account.models import User
 class RegisterUserForm(ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email']
+        fields = ['first_name', 'last_name', 'email', 'password']
+
+    def save(self, commit=True):
+        user = super(RegisterUserForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
