82ed8f098339a3046fd3595305e12aa5ef3ba35b
Romain Garrigues <romain@iwoca.co.uk>
2020-05-05 22:50:39 +0100

    Adds login/logout and first authentication protected account page.

--- legacybook_server/account/views.py
+++ legacybook_server/account/views.py
@@ -1,6 +1,21 @@
+
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
 from django.shortcuts import render
 
 from account.forms import RegisterUserForm
+
+
+class CustomLogoutView(LogoutView):
+
+    def get_next_page(self):
+        next_page = super(CustomLogoutView, self).get_next_page()
+        messages.add_message(
+            self.request, messages.SUCCESS,
+            'Vous avez été déconnecté avec success!'
+        )
+        return next_page
 
 
 def register(request):
@@ -17,3 +32,8 @@ def register(request):
     return render(request, 'account/register.html', {
         'register_form': register_form
     })
+
+
+@login_required
+def profile(request):
+    return render(request, 'account/profile.html')
