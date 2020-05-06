30535951bc98ed8132199a9fa153def5dcfd1350
Romain Garrigues <romain@iwoca.co.uk>
2020-05-06 20:10:51 +0100

    Updates registration password input field to be hidden when typing characters.

--- legacybook_server/account/forms.py
+++ legacybook_server/account/forms.py
@@ -1,12 +1,15 @@
-from django.forms import ModelForm
+from django import forms
 
 from account.models import User
 
 
-class RegisterUserForm(ModelForm):
+class RegisterUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'password']
+        widgets = {
+            'password': forms.PasswordInput(),
+        }
 
     def save(self, commit=True):
         user = super(RegisterUserForm, self).save(commit=False)
